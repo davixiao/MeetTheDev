6995aac3a4322e99c01bf020366a89dd1db01d0d
David Xiao <davidx@Huogens-MacBook-Pro.local>
2020-12-28 19:46:57 -0800

    connected to mongodb database

---
+++ config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+// config lets us grab values from anywhere.
+// we established mongoURI in the default.json, and with config
+// we can grab it like this:
+const db = config.get('mongoURI');
+
+// mongoose.connect(db); returns promise
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected');
+  } catch (err) {
+    console.error(err.message);
+    // exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
