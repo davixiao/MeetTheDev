4738f84bb65fa285a671f5be860be2a8e07b1a1d
David Xiao <avidxiao@gmail.com>
2020-12-29 12:19:11 -0800

    initial JWT implementation for user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -69,7 +71,21 @@ router.post(
 
       await user.save();
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id, // after saving, mongodb provides an _id, which mongoose simplifies to id
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      ); // 1 hour is 3600 seconds. we add a few 0s for development
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
