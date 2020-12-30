fe4e48cb13fd19954d493d280a09971103b5a993
David Xiao <avidxiao@gmail.com>
2020-12-29 21:54:34 -0800

    Adding user work experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -43,6 +43,14 @@ router.get('/me', auth, async (req, res) => {
 // we store the user id.
 // Now, remember that mongodb genreates its own ids. So each profile gets its own
 // unique id.
+/* 
+What is happening? When logged in, the user makes a post request to
+api/profile. This route deals with this request. Perhaps it would have been better
+to make it go to /api/profile/me? Doesn't matter in the end, though.
+
+Personally, I think this should have been a PUT request since we are creating/updating
+a profile. 
+*/
 router.post(
   '/',
   [
@@ -163,5 +171,53 @@ router.delete('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+//
+// @route PUT api/profile/experience
+// @desc  Add profile experience
+// @access private
+router.put(
+  '/experience',
+  [
+    auth,
+    check('title', 'Title is required').not().isEmpty(),
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From date is required').not().isEmpty(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
