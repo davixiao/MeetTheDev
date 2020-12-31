f6b388dd0d3147c153d5db6d22679ae71e46d5ab
David Xiao <avidxiao@gmail.com>
2020-12-30 21:31:02 -0800

    react router setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Navbar } from './components/layout/Navbar';
 import { Landing } from './components/layout/Landing';
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
+
 import './App.css';
 
 const App = () => (
   <Router>
     <Navbar />
+    {/* path is URI path. So, localhost:5000/register, etc */}
+    {/* switch is op. Without it, we would only be making a single page web page*/}
     <Route exact path='/' component={Landing} />
+    <section className='container'>
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+      </Switch>
+    </section>
   </Router>
 );
 
