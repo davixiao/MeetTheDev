2ec5ace94a0547c4506772592b33e992f375169c
David Xiao <avidxiao@gmail.com>
2020-12-30 21:14:35 -0800

    initial frontend set up

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Navbar } from './components/layout/Navbar';
+import { Landing } from './components/layout/Landing';
+import './App.css';
+
+const App = () => (
+  <Router>
+    <Navbar />
+    <Route exact path='/' component={Landing} />
+  </Router>
+);
+
+export default App;
