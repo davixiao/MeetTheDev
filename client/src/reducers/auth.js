95f6c0a3b3ef633952c9d1b66ddc93ec650b8d34
David Xiao <avidxiao@gmail.com>
2020-12-31 19:24:00 -0800

    added logout and dynamic navbar

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -5,6 +5,7 @@ import {
   AUTH_ERROR,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  LOGOUT,
 } from '../actions/types';
 
 const initialState = {
@@ -35,6 +36,7 @@ const authReducer = (state = initialState, { type, payload }) => {
     case AUTH_ERROR:
     case REGISTER_FAIL:
     case LOGIN_FAIL:
+    case LOGOUT:
       //localStorage.removeItem('token');
       return {
         ...state,
