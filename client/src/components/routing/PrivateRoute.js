bdfd107c56f75cfd2c9844a07c7a59f07e53496d
David Xiao <avidxiao@gmail.com>
2021-01-04 09:13:38 -0800

    fixed logic error that would display private route even when user was not authenticated. Also changed class to className

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -2,20 +2,8 @@ import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
 
-/*
-render prop is what allows two react components to share code. 
-Component with render prop takes a function (in this case, our function is (props) => ...)
-that returns a React element. It then calls it instead of returning its own render logic.
-
-In this case, we expect <Route /> to give us its own React elements. Instead, it 
-either calls login, or the passed in Component, where the passed in Component takes in
-the props that were supposed to go to <ROute />
-*/
-
-// the ...rest takes in the custom props for the specific Component
-// then, the ...rest corresponds to the props in (props) and is passed into
-// <Component />
 const PrivateRoute = ({
   component: Component,
   auth: { isAuthenticated, loading },
@@ -24,10 +12,12 @@ const PrivateRoute = ({
   <Route
     {...rest}
     render={(props) =>
-      !isAuthenticated && !loading ? (
-        <Redirect to='/login' />
-      ) : (
+      loading ? (
+        <Spinner />
+      ) : isAuthenticated ? (
         <Component {...props} />
+      ) : (
+        <Redirect to='/login' />
       )
     }
   />
